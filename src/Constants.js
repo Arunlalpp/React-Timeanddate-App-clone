a706b9181a0f19e9bfbf2c5cfcabc22343fb5020
arunlalpp <arunlalpp@gmail.com>
2022-06-15 23:41:21 +0530

    Feat:"Created the navlinks mobile responsive "

--- src/Constants.js
+++ src/Constants.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HiUserCircle } from "react-icons/hi";
 import { IoMdShare } from "react-icons/io";
 import { BiSearch } from "react-icons/bi";
+import { IoIosArrowDown } from "react-icons/io";
 
 const navItems = [
 	{ name: "Home" },
@@ -25,9 +26,9 @@ const items = [
 ];
 const navLinks = [
 	{ title: "Time/General" },
-	{ title: "Weather" },
+	{ title: "Weather", icon: <IoIosArrowDown /> },
 	{ title: "Time Zone" },
 	{ title: "DST Changes" },
-	{ title: "Sun & Moon" },
+	{ title: "Sun & Moon", icon: <IoIosArrowDown /> },
 ];
 export { navItems, items, navLinks };
--- src/components/ScrollNavlinks.js
+++ src/components/ScrollNavlinks.js
@@ -3,16 +3,21 @@ import { navLinks } from "../Constants";
 
 function ScrollNavlinks() {
 	return (
-		<div className="w-full bg-white flex flex-row">
-			{navLinks?.map((navLink) => {
-				return (
-					<div className="p-3">
-						<span className="text-[14px] font-sans whitespace-nowrap  ">
-							{navLink.title}
-						</span>
-					</div>
-				);
-			})}
+		<div className="w-full bg-[#f7f3ea]  ">
+			<div className=" flex flex-row md:overflow-hidden overflow-scroll md:max-w-[58%] m-auto">
+				{navLinks?.map((navLink) => {
+					return (
+						<div className="p-2">
+							<span className="text-[15px] md:py-[12px] font-sans  px-1 whitespace-nowrap text-[#556bb5] flex flex-row items-center">
+								{navLink.title}
+								<span className="text-black px-1 md:block hidden">
+									{navLink.icon}
+								</span>
+							</span>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
