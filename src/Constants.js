adf73b2b53807ea815489eda035bd9d662cda2fe
arunlalpp <arunlalpp@gmail.com>
2022-06-15 19:45:54 +0530

    Feat:"searchbar and the flag with constants"

---
+++ src/Constants.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { HiUserCircle } from "react-icons/hi";
+import { IoMdShare } from "react-icons/io";
+import { BiSearch } from "react-icons/bi";
+
+const navItems = [
+	{ name: "Home" },
+	{ name: "World Clock" },
+	{ name: "Time Zones" },
+	{ name: "Calendar" },
+	{ name: "Weather" },
+	{ name: "Sun & Moon" },
+	{ name: "Timers" },
+	{ name: "Calculators" },
+	{ name: "Apps & API" },
+	{ name: "Free Fun" },
+];
+
+const items = [
+	{
+		image: <HiUserCircle />,
+		icon: <IoMdShare className="text-[24px]" />,
+		search: <BiSearch className="text-[24px]" />,
+	},
+];
+export { navItems, items };
