b52c729a820da70578be921064a6abe29c4a8fb5
arunlalpp <arunlalpp@gmail.com>
2022-06-18 12:56:48 +0530

    Feat:"Added the bredcrums"

---
+++ src/components/BreadCrums.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function BreadCrums() {
+	return (
+		<div className="bg-[#12578f] ">
+			<div className="2xl:max-w-[58%] m-auto max-w-[80%]">
+				<div className="text-[11px] flex flex-row text-[#ccc] ml-1.5">
+					<span className="px-1">Home /</span>
+					<span className="px-1">Time Zome /</span>
+					<span className="px-1">World Clock /</span>
+					<span className="px-1">Singapore /</span>
+					<span className="px-1">Singapore</span>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default BreadCrums;
