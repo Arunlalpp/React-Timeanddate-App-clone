fda393b2493f62db63ac047d749054c5de3514ef
arunlalpp <arunlalpp@gmail.com>
2022-06-19 19:16:51 +0530

    Feat:Add tailwind congig and add web log

---
+++ Workspace/kl-tourism-app/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+	return (
+		<div>
+			<div className="">
+				<div className="">
+					<img
+						className="w-[186px] h-[135px]"
+						src="http://www.keralatourism.gov.in/new-style/images/kerala-logo.jpg"
+						alt=""
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
