bd2a2da8bb8e7b7d538c849a1b960c677913e039
arunlalpp <arunlalpp@gmail.com>
2022-06-19 20:15:20 +0530

    Feat: Add the navbar logos

---
+++ Workspace/kl-tourism-app/src/components/NavLink.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavLink() {
+	return (
+		<div>
+			<div className="bg-[#013567]">
+				<div className="">
+					<ul></ul>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default NavLink;
--- Workspace/kl-tourism-app/src/components/Navbar.js
+++ Workspace/kl-tourism-app/src/components/Navbar.js
@@ -3,11 +3,17 @@ import React from "react";
 function Navbar() {
 	return (
 		<div>
-			<div className="">
-				<div className="">
+			<div className=" md:flex flex-row  items-center 2xl:w-[70%] m-auto">
+				<div className="w-[100%] text-center">
 					<img
 						className="w-[186px] h-[135px]"
 						src="http://www.keralatourism.gov.in/new-style/images/kerala-logo.jpg"
+						alt=""
+					/>
+				</div>
+				<div className="">
+					<img
+						src="http://www.keralatourism.gov.in/new-style/images/title-banner.jpg"
 						alt=""
 					/>
 				</div>
