29b69ca4a3afe03c5d4a2bc32a8b2045a43d2948
arunlalpp <arunlalpp@gmail.com>
2022-06-23 00:54:50 +0530

    Feat:"Changed button text color"

--- Workspace/women-in-texh/src/components/ButtonBoard.js
+++ Workspace/women-in-texh/src/components/ButtonBoard.js
@@ -3,7 +3,7 @@ import React from "react";
 function ButtonBoard() {
 	return (
 		<div>
-			<button class="bg-[#00f4cb] hover:bg-red-600 text-[#15009b] font-bold py-2.5 px-7 rounded-full uppercase text-[15px] transition duration-150 ease-in-out">
+			<button class="bg-[#00f4cb] hover:bg-red-600 text-white font-bold py-2.5 px-7 rounded-full uppercase text-[15px] transition duration-150 ease-in-out">
 				job board
 			</button>
 		</div>
