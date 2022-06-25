fc14b935901fac4bf94152a66592b3b1cda0f650
arunlalpp <arunlalpp@gmail.com>
2022-06-25 12:48:01 +0530

    Feat:"Mobile and desk path defined"

--- Workspace/flipkart-clone/src/App.js
+++ Workspace/flipkart-clone/src/App.js
@@ -5,20 +5,22 @@ import Header from "./components/Header";
 import LeftContainer from "./components/LeftContainer";
 import MobileCard from "./components/MobileCard";
 import MobileHeader from "./components/MobileHeader";
+import MobileScrollCard from "./components/MobileScrollCard";
 import Navlink from "./components/Navlink";
 import TableConatiner from "./components/TableConatiner";
 
 function App() {
 	return (
 		<div>
-			{/* <Header />
+			<MobileHeader />
+			<MobileCard />
+			<Header />
 			<Navlink />
 			<LeftContainer />
 			<CardContainer />
 			<TableConatiner />
-			<Footer /> */}
-			<MobileHeader />
-			<MobileCard />
+			<Footer />
+			<MobileScrollCard />
 		</div>
 	);
 }
